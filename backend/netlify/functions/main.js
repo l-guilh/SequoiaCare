1591020b38fb3381c4fcb50ae14ab290577a47c7
luis <luiscardosofreitas@gmail.com>
2025-04-10 21:47:08 -0300

    Correção da estrutura do Netlify Functions

--- backend/netlify/functions/main.js
+++ backend/netlify/functions/main.js
@@ -1,5 +1,12 @@
-const { handler } = require('../../main.py')
+const { handler } = require('../../../main')
 
 exports.handler = async (event, context) => {
-  return await handler(event, context)
+  try {
+    return await handler(event, context)
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    }
+  }
 } 
